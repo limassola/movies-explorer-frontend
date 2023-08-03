7bb9fc99c78909e1e467a470944326d69136072a
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-08-03 14:27:34 +0300

    feat: адаптив для 320px страницы Movies

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -11,10 +11,6 @@ function MoviesCardList() {
                 <MoviesCard/>
                 <MoviesCard/>
                 <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-                
             </div>
             <button className="movies-card-list__button">Ещё</button>
         </div>
