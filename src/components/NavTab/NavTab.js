a3ec9d248897b078c2df06e46b7423bc8ef9b8c8
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-08-07 18:36:16 +0300

    fix: исправлена семантика блока NavTab

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -3,9 +3,11 @@ import './NavTab.css';
 function NavTab() {
   return (
     <nav className="navTab">
-      <a className='navTab__item' href="#aboutProject">О проекте</a>
-      <a className='navTab__item' href="#techs">Технологии</a>
-      <a className='navTab__item' href="#aboutMe">Студент</a>
+      <ul className='navTab__items'>
+        <a className='navTab__item' href="#aboutProject">О проекте</a>
+        <a className='navTab__item' href="#techs">Технологии</a>
+        <a className='navTab__item' href="#aboutMe">Студент</a>
+      </ul>
     </nav>
   );
 }
