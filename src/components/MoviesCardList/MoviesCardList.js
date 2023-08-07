7d61223e2288fcc601a67fa96d040e059997e5eb
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-08-07 19:20:51 +0300

    fix: исправил семантику списка фильмов

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -5,11 +5,12 @@ import './MoviesCardList.css';
 function MoviesCardList({isSavedPage}) {
     return(
         <div className={`movies-card-list ${isSavedPage ? "movies-card-list_type_save" : ''}`}>
-            <div className={`movies-card-list__container ${isSavedPage && "movies-card-list__container_type_save"}`}>
-                <MoviesCard isSavedPage={isSavedPage}/>
-                <MoviesCard isSavedPage={isSavedPage}/>
+            <ul className={`movies-card-list__container ${isSavedPage && "movies-card-list__container_type_save"}`}>
+                <li className="movies-card-list__item"><MoviesCard isSavedPage={isSavedPage}/></li>
+                <li className="movies-card-list__item"><MoviesCard isSavedPage={isSavedPage}/></li>
                 
-            </div>
+                
+            </ul>
             {isSavedPage ? null : <button className="movies-card-list__button">Ещё</button>}
         </div>
     )
