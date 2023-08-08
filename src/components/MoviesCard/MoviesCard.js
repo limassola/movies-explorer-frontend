cc38472e6bdebe5d3ff8c936c112a879097b42ac
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-08-08 15:20:52 +0300

    fix: оптимизированно фото и исправлен атрибут alt у moviesCard

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import image from '../../images/movie__image.png'
 import './MoviesCard.css';
 
-function MoviesCard({ isSavedPage }) {
+function MoviesCard({title, isSavedPage }) {
     const [isSaved, setIsSaved] = React.useState(false)
     const handleSaveClick = () => {
         setIsSaved(!isSaved)
@@ -10,7 +10,7 @@ function MoviesCard({ isSavedPage }) {
     
     return(
         <div className="movies-card">
-            <img className="movies-card__image" src={image} alt="обложка фильма"/>
+            <img className="movies-card__image" src={image} alt={title}/>
             {isSavedPage ? (<button className="movies-card__button movies-card__button_delete"></button>) : (<button onClick={handleSaveClick} className={`movies-card__button ${isSaved ? "movies-card__button_checkmark" : "movies-card__button_save"}`}>{isSaved ? null : "Сохранить"}</button>)}
             <div className="movies-card__heading">
                 <h2 className="movies-card__title">Пи Джей Харви: A dog called money</h2>
