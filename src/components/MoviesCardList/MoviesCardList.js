a3547199b63c6d5ac68747c58a19f6811e140981
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-08-09 18:03:52 +0300

    feat: добавлена динамечкая отрисовка карточек в зависимости от размера экрана

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import './MoviesCardList.css';
 
 function MoviesCardList({isSavedPage}) {
+    const movies = [
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+        <MoviesCard isSavedPage={isSavedPage}/>,
+    ];
+
+    const [visibleCards, setVisibleCards] = useState(12)
+
+    const handleResize = () => {
+        if(window.innerWidth >= 1280) {
+            setVisibleCards(12);
+        } else if (window.innerWidth >= 768) {
+            setVisibleCards(8);
+        } else if (window.innerWidth >= 320) {
+            setVisibleCards(5);
+        }
+    };
+
+    useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
+
+    const visibleMovies = movies.slice(0, visibleCards)
     return(
         <div className={`movies-card-list ${isSavedPage ? "movies-card-list_type_save" : ''}`}>
             <ul className={`movies-card-list__container ${isSavedPage && "movies-card-list__container_type_save"}`}>
-                <li className="movies-card-list__item"><MoviesCard isSavedPage={isSavedPage}/></li>
-                <li className="movies-card-list__item"><MoviesCard isSavedPage={isSavedPage}/></li>
-                <li className="movies-card-list__item"><MoviesCard isSavedPage={isSavedPage}/></li>
-                <li className="movies-card-list__item"><MoviesCard isSavedPage={isSavedPage}/></li>
-                <li className="movies-card-list__item"><MoviesCard isSavedPage={isSavedPage}/></li>
+                {visibleMovies.map((movie, index) => (
+                    <li key={index} className="movies-card-list__item">{movie}</li>
+                ))}
             </ul>
             {isSavedPage ? null : <button type='button' className="movies-card-list__button">Ещё</button>}
         </div>
