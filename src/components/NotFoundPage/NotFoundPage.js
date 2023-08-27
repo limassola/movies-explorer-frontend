3a66e19ee30e6b48f29aa26563397c8990595815
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-08-27 17:42:14 +0300

    fix: исправлена страница 404

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import './NotFoundPage.css';
 
 function NotFoundPage() {
+  const navigate = useNavigate();
+    const onReturnClick = () => {
+        navigate(-1);
+    }
   return (
     <main>
       <section>
         <div className='not-found-page'>
           <h1 className='not-found-page__title'>404</h1>
           <p className='not-found-page__text'>Страница не найдена</p>
-          <Link to="/" className='not-found-page__link'>Назад</Link>
+          <p className='not-found-page__link' onClick={onReturnClick}>Назад</p>
         </div>
       </section>
     </main>
